339342c81aeab1d41011deee873691dcdd33beb5
Théo <theo.dalgobbo@gmail.com>
2023-02-23 10:54:50 +0100

    maj dl fichier

--- apiV1/script.py
+++ apiV1/script.py
@@ -1,9 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
-y = np.array([335,125,125,115])
-mylabels = ["Apples","Bananas","Cherries","Dates"]
+# y = np.array([335,125,125,115])
+# mylabels = ["Apples","Bananas","Cherries","Dates"]
+#
+# plt.pie(y,labels=mylabels,startangle=90)
+# plt.draw()
+# plt.savefig('image.png',dpi=100)
 
-plt.pie(y,labels=mylabels,startangle=90)
-plt.draw()
-plt.savefig('image.png',dpi=100)
+
+
+
+# test mais marche pas
+# accidents_data = pd.read_csv('../data/lieux-2021.csv')
+# plt.plot(accidents_data['Num_Acc'], accidents_data['catr'])
+
+# ca affiche le graphe de base
+y = np.array([335, 125, 125, 115])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+plt.pie(y, labels = mylabels, startangle = 90)
+plt.show()
+plt.savefig("image.png")
