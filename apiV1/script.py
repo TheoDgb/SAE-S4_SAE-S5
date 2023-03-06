dd34a5af29a7cfbbebcf841b8eb9fb1b436c75d4
dal gobbo theo <theo.dal_gobbo@edu.univ-fcomte.fr>
2023-03-06 10:43:39 +0100

    Data frame pour années 2019 - 2020 - 2021 Correction du problème server.js qui fermait l'app une fois l'execution du script python terminé

--- apiV1/script.py
+++ apiV1/script.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 ################################# Importation des fichiers CSV #################################
 
-file_names = ['2021/usagers-2021.csv', '2021/vehicules-2021.csv', '2021/lieux-2021.csv', '2021/caracteristiques-2021.csv']
+file_names = ['./data/2021/usagers-2021.csv', './data/2021/vehicules-2021.csv', './data/2021/lieux-2021.csv', './data/2021/caracteristiques-2021.csv']
 donnees_2021 = pd.read_csv(file_names[0])
 
 for file in file_names[1:]:
@@ -15,7 +15,7 @@ for file in file_names[1:]:
 donnees_2021 = donnees_2021.sort_values(by='id').reset_index(drop=True)
 
 
-file_names = ['2020/usagers-2020.csv', '2020/vehicules-2020.csv', '2020/lieux-2020.csv', '2020/caracteristiques-2020.csv']
+file_names = ['./data/2020/usagers-2020.csv', './data/2020/vehicules-2020.csv', './data/2020/lieux-2020.csv', './data/2020/caracteristiques-2020.csv']
 donnees_2020 = pd.read_csv(file_names[0])
 
 for file in file_names[1:]:
@@ -24,7 +24,7 @@ for file in file_names[1:]:
 
 donnees_2020 = donnees_2020.sort_values(by='id').reset_index(drop=True)
 
-file_names = ['2019/usagers-2019.csv', '2019/vehicules-2019.csv', '2019/lieux-2019.csv', '2019/caracteristiques-2019.csv']
+file_names = ['./data/2019/usagers-2019.csv', './data/2019/vehicules-2019.csv', './data/2019/lieux-2019.csv', './data/2019/caracteristiques-2019.csv']
 donnees_2019 = pd.read_csv(file_names[0])
 
 for file in file_names[1:]:
@@ -43,12 +43,28 @@ donnees_2021 = donnees_2021.loc[donnees_2021['sexe'] != -1]
 
 # afficher un graphe de la répartition des usagers par sexe
 sns.countplot(x='sexe', data=donnees_2021)
-plt.title('Répartition des usagers par sexe')
+plt.title('Répartition des usagers par sexe en 2021')
 plt.xlabel('Sexe')
 plt.ylabel('Nombre d\'usagers')
-plt.xticks([0, 1, 2], ['Non renseigné','Homme', 'Femme'])
-plt.savefig("./public/images/image.png")
-plt.show()
+plt.xticks([0, 1], ['Homme', 'Femme'])
+plt.savefig("./public/images/image_sexe2021.png")
+# plt.show()
+
+sns.countplot(x='sexe', data=donnees_2020)
+plt.title('Répartition des usagers par sexe en 2020')
+plt.xlabel('Sexe')
+plt.ylabel('Nombre d\'usagers')
+plt.xticks([0, 1], ['Homme', 'Femme'])
+plt.savefig("./public/images/image_sexe2020.png")
+# plt.show()
+
+sns.countplot(x='sexe', data=donnees_2019)
+plt.title('Répartition des usagers par sexe en 2019')
+plt.xlabel('Sexe')
+plt.ylabel('Nombre d\'usagers')
+plt.xticks([0, 1], ['Homme', 'Femme'])
+plt.savefig("./public/images/image_sexe2019.png")
+# plt.show()
 
 #Affiche un graphe avec le type de collisions
 sns.countplot(x='col', data=donnees_2021)
@@ -56,7 +72,6 @@ plt.title('Type de collisions')
 plt.xlabel('Type de collisions')
 plt.ylabel('Nombre de collisions')
 plt.xticks([0, 1, 2, 3, 4, 5, 6, 7], ['Non renseigné', 'Deux véhicules - frontale', 'Deux véhicules – par l’arrière',
-                                       'Deux véhicules – par le coté', 'Trois véhicules et plus – en chaîne', 
+                                       'Deux véhicules – par le coté', 'Trois véhicules et plus – en chaîne',
                                        'Trois véhicules et plus - collisions multiples', 'Autre collision',
                                        'Sans collision'], rotation=90)
-plt.show()
--- apiV1/server.js
+++ apiV1/server.js
@@ -13,99 +13,65 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(express.static('public'));
 
+// ROUTES
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
-
 app.get('/swagger', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'swagger.html'));
 });
-
+// PORT
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 });
 
-
-
-// let {PythonShell} = require('python-shell');
-//
-// const options = {
-//     scriptPath: '',
-//     args: ['arg1', 'arg2', 'arg3']
-// };
-//
-// PythonShell.run('script.py', options, function (err, results) {
-//     if (err) throw err;
-//     console.log('results: %j', results);
-// });
-
-
-//EN BINAIRE
-// let {PythonShell} = require('python-shell');
-// const fs = require('fs');
-//
-// const options = {
-//     scriptPath: '',
-//     args: ['arg1', 'arg2', 'arg3']
-// };
-//
-// PythonShell.run('script.py', options, function (err) {
-//     if (err) throw err;
-//     fs.readFile('image.png', (err, data) => {
-//         if (err) throw err;
-//         console.log(data);
-//     });
-// });
-
-// !openssl enc -base64 -d -in 'ratio.bmp' -out 'zebi.bmp'
-
-
 // EN BASE 64
-let {PythonShell} = require('python-shell');
+const {PythonShell} = require('python-shell');
 const fs = require('fs');
 
 const options = {
     scriptPath: '',
     args: ['arg1', 'arg2', 'arg3']
 };
-
 PythonShell.run('script.py', options, function (err) {
     if (err) throw err;
-    fs.readFile('image.png', (err, data) => {
+    fs.readFile('./public/images/image.png', (err, data) => {
         if (err) throw err;
-        const base64Image = new Buffer.from(data, 'binary').toString('base64');
+        const base64Image = Buffer.from(data, 'binary').toString('base64');
         const imgSrc = `data:image/png;base64,${base64Image}`;
         console.log(imgSrc);
     });
 });
 
+// deuxieme script au cas ou
+// PythonShell.run('script2.py', options, function (err) {
+//     if (err) throw err;
+//     fs.readFile('./public/images/image2.png', (err, data) => {
+//         if (err) throw err;
+//         const base64Image = Buffer.from(data, 'binary').toString('base64');
+//         const imgSrc = `data:image/png;base64,${base64Image}`;
+//         console.log(imgSrc);
+//     });
+// });
+
 // télécharger des datasets en csv depuis opendaatsoft sans axios des accidents corporels de la circulation millésimé:
 
-
 const https = require('https');
-// const file = fs.createWriteStream("./data/accidents.csv");
-// const request = https.get("https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/accidents-corporels-de-la-circulation-millesime/exports/csv?lang=fr&timezone=Europe%2FBerlin&use_labels=true&csv_separator=%3B", function(response) {
-//     response.pipe(file);
-// });
-//
-// const file2 = fs.createWriteStream("./data/accidents2.csv");
-// const request2 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
-//     response.pipe(file2);
-// });
+// A FAIRE : DL AUTO (MAUVAIS LIENS)
 
-const filecaracteristiques2021 = fs.createWriteStream("./data/accidents2.csv");
-const request1 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
-    response.pipe(filecaracteristiques2021);
-});
-const filelieux2021 = fs.createWriteStream("./data/accidents2.csv");
-const request2 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
-    response.pipe(filelieux2021);
-});
-const fileusagers2021 = fs.createWriteStream("./data/accidents2.csv");
-const request3 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
-    response.pipe(fileusagers2021);
-});
-const filevehicules2021 = fs.createWriteStream("./data/accidents2.csv");
-const request4 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
-    response.pipe(filevehicules2021);
-});
+// const filecaracteristiques2021 = fs.createWriteStream("./data/accidents2.csv");
+// const request1 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
+//     response.pipe(filecaracteristiques2021);
+// });
+// const filelieux2021 = fs.createWriteStream("./data/accidents2.csv");
+// const request2 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
+//     response.pipe(filelieux2021);
+// });
+// const fileusagers2021 = fs.createWriteStream("./data/accidents2.csv");
+// const request3 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
+//     response.pipe(fileusagers2021);
+// });
+// const filevehicules2021 = fs.createWriteStream("./data/accidents2.csv");
+// const request4 = https.get("https://static.data.gouv.fr/resources/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2020/20221024-114007/usagers-2021.csv", function(response) {
+//     response.pipe(filevehicules2021);
+// });
